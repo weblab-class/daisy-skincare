c4a77f5406b95895f144f007686583191cf989ca
Grace Choi <gracechoi@dhcp-10-31-137-194.dyn.mit.edu>
2026-01-28 02:47:32 -0500

    grace home headings

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -22,14 +22,16 @@ const Home = () => {
           <main className="relative flex flex-col items-center justify-start min-h-screen text-center pt-5 px-4">
             <div className="space-y-5">
               <h1 className="relative px-7 py-2">
-                <span className="absolute inset-0 bg-[#a3d0c9]/30 backdrop-blur-md rounded-xl shadow-[0_0_20px_rgba(0,212,255,0.5)]"></span>
+                <span className="absolute inset-0 backdrop-blur-sm rounded-xl"></span>
                 <span className="text-7xl md:text-15xl font-serif text-white tracking-tighter italic drop-shadow-lg">
                   Daisy
                 </span>
               </h1>
-              <p className="text-white/90 font-black text-sm md:text-lg uppercase tracking-[0.4em] drop-shadow-sm antialiased tracking-tight text-shadow">
-                Your social network <br /> for skincare sidequests
-              </p>
+                <p className="text-white/90 inline font-black text-xl md:text-lg uppercase tracking-[0.4em]
+                drop-shadow-sm antialiased tracking-tight text-shadow">
+                  <span className="absolute inset-0 bg-white/10 backdrop-blur-md rounded-xl "></span>
+                  Your social network for skincare sidequests
+                </p>
             </div>
           </main>
         )}
